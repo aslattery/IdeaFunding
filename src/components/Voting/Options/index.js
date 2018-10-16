b86474c40c2a94a7cb5027ebb3358c8714579758
Andrew Slattery <andrew@hivemetric.com>
2018-10-15 20:10:33 -0700

    Pitch buttons

--- src/components/Voting/Options/index.js
+++ src/components/Voting/Options/index.js
@@ -2,10 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './styles.less';
+import VotingButton from './../Button';
 
-const VotingOptions = () => (
-    <div />
-);
+const opts = [
+    'PITCH1',
+    'PITCH2',
+    'PITCH3',
+    'PITCH4',
+    'PITCH5',
+    'PITCH6'
+];
+
+const VotingOptions = () => {
+    let optionButtons = [];
+    opts.map((opt) => {
+        optionButtons.push(<VotingButton code={opt} />);
+    });
+    return (
+        <div className="options">
+            {optionButtons}
+        </div>
+    );
+};
 
 VotingOptions.propTypes = {};
 
