aa5840555e6ddba72f1a2be0ab547b13eab46ac5
Andrew Slattery <andrew@hivemetric.com>
2018-10-15 20:31:11 -0700

    Init results page

---
+++ src/pages/results.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+import DefaultLayout from './../layouts/unauthenticated';
+import VotingStatCards from './../components/Voting/StatCards';
+import VotingOptions from './../components/Voting/Options';
+import VotingRules from './../components/Voting/Rules';
+
+class ResultsPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            totalVotes: 0,
+            uniqueVotes: 0,
+        };
+    }
+
+    render() {
+        return (
+            <DefaultLayout>
+                <Helmet>
+                    <title>IdeaFunding 2018 Results</title>
+                </Helmet>
+                <main>
+                    <VotingStatCards
+                        votes={this.state.totalVotes}
+                        uniques={this.state.uniqueVotes}
+                    />
+                </main>
+            </DefaultLayout>
+        );
+    }
+
+}
+
+export default ResultsPage;
